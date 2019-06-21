acea481aeb6f6c655971bfa20b970997bf93a0fb
ShowiGore <david444488888888@gmail.com>
2019-06-21 18:36:33 +0200

    Cube_3x3x3 added toString

--- src/Cube_3x3x3.java
+++ src/Cube_3x3x3.java
@@ -1,3 +1,103 @@
+import com.sun.source.tree.ReturnTree;
+
 public class Cube_3x3x3 {
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	private String[] color = {"\u001B[40m"+ANSI_RED, "\u001B[40m"+ANSI_PURPLE, "\u001B[40m"+ANSI_WHITE, "\u001B[40m"+ANSI_YELLOW, "\u001B[40m"+ANSI_BLUE, "\u001B[40m"+ANSI_GREEN}; // colors
+
+	private int row = 3;
+	private int column = 3;
+
+	private int[][] F = new int[row][column];
+	private int[][] B = new int[row][column];
+	private int[][] U = new int[row][column];
+	private int[][] D = new int[row][column];
+	private int[][] L = new int[row][column];
+	private int[][] R = new int[row][column];
+
+	public Cube_3x3x3() {
+
+
+
+		filler(F, 0);
+		filler(B, 1);
+		filler(U, 2);
+		filler(D, 3);
+		filler(L, 4);
+		filler(R, 5);
+
+	}
+
+	private int[][] filler (int[][] f, int c) { // Rellena una cara de un color
+
+		for (int i=0; i<row; i++){
+			for (int j=0; j<column; j++){
+				f[i][j] = c;
+			}
+		}
+
+		return f;
+	}
+
+	/*
+
+		UUU
+		UUU
+		UUU
+	LLL	FFF	RRR	BBB
+	LLL FFF	RRR	BBB
+	LLL FFF	RRR	BBB
+		DDD
+		DDD
+		DDD
+
+	*/
+
+	public String toString () {
+		String padding = "\u001B[40m"+ANSI_BLACK+"■■■"+"\u001B[38m";
+		String s = "";
+
+		for (int i=0; i<row; i++){
+			s += padding;
+			for (int j=0; j<column; j++){
+				s += color[U[i][j]]+"■"+ANSI_RESET;
+			}
+			s += padding+padding+"\n";
+		}
+
+		for (int i=0; i<row; i++){
+			for (int j=0; j<column; j++){
+				s += color[L[i][j]]+"■"+ANSI_RESET;
+			}
+			for (int j=0; j<column; j++){
+				s += color[F[i][j]]+"■"+ANSI_RESET;
+			}
+			for (int j=0; j<column; j++){
+				s += color[R[i][j]]+"■"+ANSI_RESET;
+			}
+			for (int j=0; j<column; j++){
+				s += color[B[i][j]]+"■"+ANSI_RESET;
+			}
+			s += "\n";
+		}
+
+		for (int i=0; i<row; i++){
+			s += padding;
+			for (int j=0; j<column; j++){
+				s += color[D[i][j]]+"■"+ANSI_RESET;
+			}
+			s += padding+padding+"\n";
+		}
+
+		return s;
+	}
 
 }
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main (String args[]) {
+
+		Cube_3x3x3 cube = new Cube_3x3x3();
+
+		System.out.println(cube.toString());
+
+		//System.out.println("\033[0;31m"+"■");
+
+	}
+
+}
